0d3275f3e8ca0036b4ac421b64445a8b2196ec41
ephrata bek <ephratake@gmail.com>
2024-09-05 11:00:04 -0700

    main

--- lib/app_pages/order_success/success_controller.dart
+++ lib/app_pages/order_success/success_controller.dart
@@ -1,41 +1,41 @@
-// import 'package:get/get.dart';
-// import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
 
-// class OrderSuccessController extends GetxController with GetSingleTickerProviderStateMixin {
-//   late AnimationController _animationController;
-//   late Animation<double> _animation;
+class OrderSuccessController extends GetxController with GetSingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
 
-//   @override
-//   void onInit() {
-//     super.onInit();
+  @override
+  void onInit() {
+    super.onInit();
 
-//     // Initialize the AnimationController and set its duration
-//     _animationController = AnimationController(
-//       vsync: this,
-//       duration: const Duration(seconds: 3),
-//     );
+    // Initialize the AnimationController and set its duration
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
 
-//     _animation = Tween<double>(begin: 0.0, end: 1.0).animate(
-//       CurvedAnimation(
-//         parent: _animationController,
-//         curve: Curves.elasticOut, // bounce effect
-//       ),
-//     );
+    _animation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.elasticOut, // bounce effect
+      ),
+    );
 
    
-//     _animationController.forward();
+    _animationController.forward();
 
-//     Future.delayed(const Duration(seconds: 2), () {
-//       Get.offAll(PageView()); 
-//     });
-//   }
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.offAll(PageView()); 
+    });
+  }
 
-//   @override
-//   void onClose() {
+  @override
+  void onClose() {
    
-//     _animationController.dispose();
-//     super.onClose();
-//   }
+    _animationController.dispose();
+    super.onClose();
+  }
 
-//   Animation<double> get animation => _animation;
-// }
+  Animation<double> get animation => _animation;
+}
